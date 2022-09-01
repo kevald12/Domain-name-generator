8a10fd3b96ef7bb4c6c9a9547e00f05fd99b44af
kevald12 <kealable@gmail.com>
2022-09-01 02:31:49 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let domainName = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombre = ["El", "La", "Los"];
+
+  let adjetivo = ["gran (des)", "pequeño (s)", "rapido (dos)", "lento (s)"];
+
+  let sustantivo = ["personas", "pintor", "niños", "tortuga", "raton", "libro"];
+
+  let dominio = [".com", ".net"];
+
+  for (let p = 0; p < pronombre.length; p++) {
+    for (let a = 0; a < adjetivo.length; a++) {
+      for (let s = 0; s < sustantivo.length; s++) {
+        for (let l = 0; l < dominio.length; l++)
+          domainName.push(
+            " " +
+              pronombre[p] +
+              " " +
+              adjetivo[a] +
+              " " +
+              sustantivo[s] +
+              " " +
+              " " +
+              dominio[l]
+          );
+      }
+    }
+  }
+  console.log(domainName);
+  document.getElementById("domain.results").innerHTML = domainName;
 };
